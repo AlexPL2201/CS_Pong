9487611d3a28a8bb5dbb61fd03a2b393185f4865
Plakhotnik Alexandr <alexpl2003@mail.ru>
2023-12-20 00:26:02 +0300

    Added registration and validation of username

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import socket
 import sqlite3
+from datetime import datetime as dt
 
-
+db_path = 'db.db'
 ip = '127.0.0.1'
 port = 9000
 addr = (ip, port)
@@ -12,31 +13,32 @@ def launch_server():
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind(addr)
-        print(f'[!] The server is running. IP: {ip} Port {port}')
+        print(f'[!] <{dt.now()}> The server is running. IP: {ip} Port {port}')
         return sock
 
     except socket.error as e:
-        print(f"[ERR] Socket binding error: {e}")
+        print(f"[ERR] <{dt.now()}> Socket binding error: {e}")
         sock.close()
     return -1
 
 
 def server_listen(sock):
     try:
-        print('[!] The server waiting for connection')
+        print(f'[!] <{dt.now()}> The server waiting for connection')
         sock.listen(clients_count)
         while True:
             client_sock, client_address = sock.accept()
-            print(f'[!] Connected! Client`s address: {client_address}')
+            print(f'[!] <{dt.now()}> Connected! Client`s address: {client_address}')
             data = client_sock.recv(1024).decode('utf-8')
             command, arguments = parse_message(data)
-            print(f'Command: {command}\nArguments: {arguments[0]} {arguments[1]}')
-            exec_command(command, arguments)
-            client_sock.send(f'Hello from server {ip}'.encode('utf-8'))
-            print(f'[!] Hello message has been sent: {client_address}')
+            print(f'[!] Message received! Command: {command} | Arguments: {arguments[0]} {arguments[1]}')
+            status, log = exec_command(command, arguments)
+            answer = f'Status: {status} | Message: {log}'
+            client_sock.send(answer.encode('utf-8'))
+            print(f'[!] <{dt.now()}> Answer has been sent: {answer}')
             client_sock.shutdown(socket.SHUT_WR)
     except KeyboardInterrupt:
-        print('[END] The server has shut down')
+        print(f'[END] <{dt.now()}> The server has shut down')
         server.close()
 
 
@@ -48,16 +50,39 @@ def parse_message(msg):
 def exec_command(cmd, args):
     if cmd == 'LOGIN':
         login()
-    if cmd == 'REGISTER':
-        register()
+    elif cmd == 'REGISTER':
+        return register(args)
 
 
 def login():
     pass
 
 
-def register():
-    pass
+def register(args):
+    data = (args[0], args[1], 0, 0, 0, dt.now())
+    if not is_player_name_unique(args[0]):
+        return 0, f"[ERR] <{dt.now()}> PlayerName '{args[0]}' is already in use"
+    con = sqlite3.connect(db_path)
+    cursor = con.cursor()
+    cursor.execute(f"""INSERT INTO Player
+        (PlayerName, HashPassword, Score, TotalGamesPlayed, TotalGamesWon, RegistrationDate)
+        VALUES (?, ?, ?, ?, ?, ?)""", data)
+    con.commit()
+    con.close()
+    return 1, f"[SUCCESS] <{dt.now()}> PlayerName '{args[0]}' has been successfully registered"
+
+
+def is_player_name_unique(player_name):
+    con = sqlite3.connect(db_path)
+    cursor = con.cursor()
+    cursor.execute('SELECT PlayerName FROM Player WHERE PlayerName = ?', (player_name,))
+
+    # Получаем первую строку результата
+    existing_player = cursor.fetchone()
+    con.close()
+
+    # Возвращаем True, если имя уникально, и False в противном случае
+    return existing_player is None
 
 
 if __name__ == '__main__':
